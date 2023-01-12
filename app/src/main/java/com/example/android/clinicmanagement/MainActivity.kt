d8475e0765021f10160bab1c610812cf97aae7e8
KarimBer <berouayakarim@gmail.com>
2023-01-12 02:21:41 +0100

    add dependency for MpAndroidChart librairy and trying it

--- app/src/main/java/com/example/android/clinicmanagement/MainActivity.kt
+++ app/src/main/java/com/example/android/clinicmanagement/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.android.clinicmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.Chart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val entries = mutableListOf<BarEntry>()
+        entries.add(BarEntry(0f, 30f))
+        entries.add(BarEntry(1f, 80f))
+        entries.add(BarEntry(2f, 60f))
+        entries.add(BarEntry(3f, 50f))
+        // gap of 2f
+        entries.add(BarEntry(5f, 70f))
+        entries.add(BarEntry(6f, 60f))
+        val chart = findViewById<BarChart>(R.id.chart)
+
+         val set:BarDataSet = BarDataSet(entries, "BarDataSet")
+        val data:BarData = BarData(set)
+        data.setBarWidth(0.9f); // set custom bar width
+        chart.setData(data);
+        chart.setFitBars(true); // make the x-axis fit exactly all bars
+        chart.invalidate(); // refresh
     }
 }
