2b4b168ab204739b5f05b3f90bddc8598902199b
KarimBer <berouayakarim@gmail.com>
2023-01-26 19:05:44 +0100

    Add ExpendituresFragment & ExpendituresCategoryAdapter

---
+++ app/src/main/java/com/example/android/clinicmanagement/ExpenditureCategoryAdapter.kt
@@ -0,0 +1,101 @@
+package com.example.android.clinicmanagement
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.clinicmanagement.databinding.ListItemExpenditureCategorySelectionBinding
+
+class ExpenditureCategoryAdapter : RecyclerView.Adapter<ExpenditureCategoryAdapter.ViewHolder>() {
+
+
+    private var data = listOf(
+        "Utilities",
+        "Rent",
+        "Repair",
+        "Insurance",
+        "Wages",
+        "Tax",
+        "Supplies",
+        "Divers"
+    )
+
+    override fun getItemCount() = data.size
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+
+    class ViewHolder private constructor(val binding: ListItemExpenditureCategorySelectionBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: String) {
+            binding.imageIcon.setImageResource(
+                when (data) {
+                    "Utilities" -> R.drawable.ic_expenditure_receipt
+                    "Rent" -> R.drawable.ic_expenditure_home
+                    "Repair" -> R.drawable.ic_expenditure_home_repair
+                    "Insurance" -> R.drawable.ic_expenditure_insurance
+                    "Wages" -> R.drawable.ic_expenditure_wallet
+                    "Tax" -> R.drawable.ic_expenditure_tax
+                    "Supplies" -> R.drawable.ic_expenditure_medical_supplies
+                    "Divers" -> R.drawable.ic_expenditure_divers
+                    else -> R.drawable.ic_expenditure_divers
+                }
+            )
+            binding.textExpenditureCategory.text = data
+            binding.cardView.setOnClickListener {
+                selectionColor?.let { color ->
+                    it.backgroundTintList = ColorStateList.valueOf(color)
+                }
+
+                previousItemPosition?.let {
+                    val viewHolder = recyclerView?.findViewHolderForAdapterPosition(it)
+                    viewHolder?.itemView?.findViewById<View>(R.id.cardView)?.backgroundTintList = null
+                }
+                selectedItem = data
+                previousItemPosition = bindingAdapterPosition
+            }
+        }
+
+        companion object {
+            var selectionColor: Int? = null
+            var recyclerView: RecyclerView? = null
+            var previousItemPosition: Int? = null
+            var selectedItem :String? = null
+            fun from(parent: ViewGroup): ViewHolder {
+                if (recyclerView == null) {
+                    recyclerView = parent as RecyclerView
+
+                    selectionColor = Color.parseColor(
+                        "#" + Integer.toHexString(
+                            ContextCompat.getColor(
+                                parent.context,
+                                R.color.color_secondary_variant_20
+                            )
+                        )
+                    )
+
+                }
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding: ListItemExpenditureCategorySelectionBinding = DataBindingUtil.inflate(
+                    layoutInflater, R.layout.list_item_expenditure_category_selection, parent, false
+                )
+
+                return ViewHolder(binding)
+            }
+        }
+
+    }
+}
+
+
---
+++ app/src/main/java/com/example/android/clinicmanagement/expenditures/ExpendituresFragment.kt
@@ -0,0 +1,56 @@
+package com.example.android.clinicmanagement.expenditures
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.clinicmanagement.ExpenditureCategoryAdapter
+import com.example.android.clinicmanagement.R
+import com.example.android.clinicmanagement.databinding.FragmentExpendituresBinding
+import com.example.android.clinicmanagement.databinding.FragmentStatisticsBinding
+
+
+class ExpendituresFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: FragmentExpendituresBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_expenditures, container, false
+        )
+
+        val manager = GridLayoutManager(activity, 3)
+        binding.listExpenditureCategory.layoutManager = manager
+
+        val adapter = ExpenditureCategoryAdapter()
+        binding.listExpenditureCategory.adapter = adapter
+
+        binding.buttonSave.setOnClickListener {
+            if(ExpenditureCategoryAdapter.ViewHolder.selectedItem != null && binding.textFieldAmount.text.toString() != ""  ) {
+                Toast.makeText(
+                    requireContext(),
+                    "Item clicked is: ${ExpenditureCategoryAdapter.ViewHolder.selectedItem}" +
+                            " Amount is :${binding.textFieldAmount.text}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }else
+                Toast.makeText(
+                    requireContext(),
+                    "field is empty or item not selected",
+                    Toast.LENGTH_LONG
+                ).show()
+        }
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+
+}
