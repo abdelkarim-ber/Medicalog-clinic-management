e8cfaf76d131c19eb000c16c23f49fbbe0db0274
KarimBer <berouayakarim@gmail.com>
2023-02-16 19:07:00 +0100

    setting up receipt fragment

---
+++ app/src/main/java/com/example/android/clinicmanagement/receipt/ReceiptFragment.kt
@@ -0,0 +1,28 @@
+package com.example.android.clinicmanagement.receipt
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.clinicmanagement.R
+import com.example.android.clinicmanagement.databinding.FragmentPatientHistoryBinding
+import com.example.android.clinicmanagement.databinding.FragmentReceiptBinding
+import com.example.android.clinicmanagement.patientHistory.PatientHistoryAdapter
+
+class ReceiptFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentReceiptBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_receipt, container, false
+        )
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
